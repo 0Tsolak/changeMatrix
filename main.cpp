49596817d3f00f4d84c6dbb30ffe0f15bbca499c
00kar <87019930+00kar@users.noreply.github.com>
2021-07-06 22:03:50 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,15 +2,18 @@
 
 
 int main() {
-
+	// Initializing a matrix
 	int a[row][col] = {{1,2,3,4,5}, {5,6,15,8,2}, {9,5,2,1,8}, {3,1,5,6,0}, {4,0,21,5,9}};
 	
+	// Printing Given matrix
 	std::cout << "Given matix\n\n";
 	print(a);
-
+	
+	// Calling this function to make changes in the matrix
 	change(a);
 
-	std::cout << "\nCanged array\n\n";
+	// Printing already changed matrix
+	std::cout << "\nCanged matrix\n\n";
 	print(a);
 
 	return 0;
