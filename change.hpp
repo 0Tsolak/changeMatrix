6e932b50c5f394712dc64667fd42d24e92e7b600
00kar <87019930+00kar@users.noreply.github.com>
2021-07-06 22:51:42 +0400

    Update change.hpp

--- change.hpp
+++ change.hpp
@@ -1,12 +1,22 @@
+// Including necessary libraries
 #include <iostream>
 #include <math.h>
+
+// Declaring and Initializing the row and column counts of matrix
 const int row = 5;
 const int col = 5;
+
+// Declaring all functions we'll use in project
+int length(int);
+
 void print(int[][col]);
+
+int DecimalToBinary(int);
+
 int sumUp(int[][col]);
 int sumDown(int[][col]);
-void change(int[][col]);
-int DecimalToBinary(int);
-int length(int);
+
 void changeUp(int[][col], int);
 void changeDown(int[][col], int);
+
+void change(int[][col]);
